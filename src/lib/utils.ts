0f34671a3a6550f86da987734a4ed06920e41f44
Osokoya Fiyinfoluwa David <osokoyafiyinfoluwa@gmail.com>
2026-01-22 21:00:14 +0100

    Update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function extractVideoId(url: string): string | null {
+export const extractVideoId = (url: string): string | null =>{
   const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
   const match = url.match(regExp)
   return match && match[2].length === 11 ? match[2] : null
