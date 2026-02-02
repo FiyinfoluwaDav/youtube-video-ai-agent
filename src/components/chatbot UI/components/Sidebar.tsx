42f8f9cdf52d71593e280abe3e674d57da9e1304
Osokoya Fiyinfoluwa David <osokoyafiyinfoluwa@gmail.com>
2026-02-02 21:10:04 +0100

    Fix the  screen size for the logo on sidebar

--- src/components/chatbot UI/components/Sidebar.tsx
+++ src/components/chatbot UI/components/Sidebar.tsx
@@ -25,7 +25,7 @@ const Sidebar = ({
 
   return (
     <div
-      className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl
+      className={`flex flex-col h-full min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl
         transition-all duration-500 max-md:absolute left-0 z-1 ${!isMenuOpen && 'max-md:-translate-x-full'}`}
     >
       {/*Logo */}
