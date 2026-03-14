d1ee1ce02602110b01cbe66b7ef7df1b7d030cad
Osokoya Fiyinfoluwa David <osokoyafiyinfoluwa@gmail.com>
2026-03-14 10:04:09 +0100

    Update trpc call

--- src/integrations/trpc/react.ts
+++ src/integrations/trpc/react.ts
@@ -1,4 +1,7 @@
-import { createTRPCContext } from '@trpc/tanstack-react-query'
 import type { TRPCRouter } from '@/integrations/trpc/router'
+import { createTRPCContext } from '@trpc/tanstack-react-query'
 
-export const { TRPCProvider, useTRPC } = createTRPCContext<TRPCRouter>()
+const trpcContext = createTRPCContext<TRPCRouter>()
+
+export const { TRPCProvider, useTRPC, useTRPCClient } = trpcContext
+export const trpc = trpcContext
